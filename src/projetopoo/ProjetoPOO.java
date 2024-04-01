7b548d50c7207c840650329d281db427988ac54f
Kaua <kaua_a.val@outlook.com>
2024-04-01 19:31:02 -0300

    Initial commit

---
+++ src/projetopoo/ProjetoPOO.java
@@ -0,0 +1,12 @@
+package projetopoo;
+
+public class ProjetoPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
